8087875be3e1c1f922d00fdbbc28ec1602e12fb8
Nathan Rowbottom <nathan.rowbottom@snpsteam.com>
2020-11-13 22:01:53 -0500

    Create Vector.java

---
+++ src/main/java/com/battlesnake/starter/Vector.java
@@ -0,0 +1,19 @@
+
+package com.battlesnake.starter;
+
+public class Vector{
+
+  int x, y;
+
+  public Vector(){
+    x = 0;
+    y = 0; 
+  }
+
+  public Vector(int _x, int _y){
+    x = _x;
+    y = _y; 
+  }
+
+
+}
